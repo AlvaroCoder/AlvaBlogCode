2c0cf3370ca6a17558dc33be188dc7e63c4c44f7
alvaro <alvaro89_piura@hotmail.com>
2024-05-22 01:08:40 -0500

    agregar adsense

--- src/models/Blog/ModelBlog.js
+++ src/models/Blog/ModelBlog.js
@@ -10,8 +10,6 @@ export default function ModelBlog({
     <main className='flex-1 flex items-center justify-center bg-black_1 px-10' >
       <section className=''>
         <LayoutUpper mainContentPost={mainContentPost} topContentPosts={topContentPost} />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7011219024368193"
-        crossorigin="anonymous"></script>
         <LayoutContentBody contentPosts={contentPostsBody}/>
       </section>
     </main>
